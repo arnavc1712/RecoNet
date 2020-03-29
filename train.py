2c3261e2f55c63a704ea211795df409654067b5d
arnavc1712 <arnav171296@gmail.com>
2020-03-28 17:23:35 -0700

    Added scheduled optimizer and gradeitn clipping

--- train.py
+++ train.py
@@ -111,8 +111,8 @@ def main(opt):
 
 	# optimizer = optim.Adam(filter(lambda x: x.requires_grad, model.parameters()),
  #                                          betas=(0.9, 0.98), eps=1e-09)
- 	optimizer = ScheduledOptim(optim.Adam(filter(lambda x: x.requires_grad, model.parameters()),
-                                          betas=(0.9, 0.98), eps=1e-09), opt["dim_model"], opt["warm_up_steps"])
+	optimizer = ScheduledOptim(optim.Adam(filter(lambda x: x.requires_grad, model.parameters()),
+	betas=(0.9, 0.98), eps=1e-09), opt["dim_model"], opt["warm_up_steps"])
 	train(dataloader,model,optimizer,opt)
 
 
