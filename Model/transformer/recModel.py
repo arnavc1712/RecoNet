7f04719c9640f3cca8b03ab72bf33fd29d6a5811
arnavc1712 <arnav171296@gmail.com>
2020-04-05 12:47:33 -0700

    Changed directory

--- Model/transformer/recModel.py
+++ Model/transformer/recModel.py
@@ -94,7 +94,7 @@ class Encoder(nn.Module):
             EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
             for _ in range(n_layers)])
 
-    def user_representation(self,src_emb,src_pos,user_ids,return_attns=False):
+    def user_representation(self,src_emb,src_pos,user_ids,return_attns=False,include_user=0):
 
         ''' 
             src_emb: Sequences of items (batch_size,max_batch_len)
@@ -102,7 +102,7 @@ class Encoder(nn.Module):
 
         '''
         enc_slf_attn_list = []
-        print(user_ids.size())
+      
         ## We do not need padding masks as the video clips are 40 frames each
 
         # -- Prepare masks
@@ -113,35 +113,41 @@ class Encoder(nn.Module):
  
         src_item_emb = self.item_embedding(src_emb) ## (Batch_size, max_seq_len, dim_item)
         # -- Forward
-        src_user_emb = self.user_embedding(user_ids) ## (Batch_size,dim)
-        # print(src_user_emb.size())
-        src_user_emb = src_user_emb.unsqueeze(1).expand(*src_item_emb.size()).contiguous()
+
+        if include_user:
+            src_user_emb = self.user_embedding(user_ids) ## (Batch_size,dim)
+            # print(src_user_emb.size())
+            src_user_emb = src_user_emb.unsqueeze(1).expand(*src_item_emb.size()).contiguous()
+            src_user_emb *= non_pad_mask
+       
         # print(src_user_emb.size())
        
         enc_output = src_item_emb + self.position_enc(src_pos) ## (Batch_size, max_batch_len, dim_item)
 
-        enc_output = enc_output 
+ 
         ## Converting dimensions of video + postional embeddings into d_model
         batch_size, seq_len, dim_item = enc_output.size()
 
-        enc_output = enc_output.view(-1, dim_item)
+        # enc_output = enc_output.view(-1, dim_item)
 
-        enc_output = self.item2hid(enc_output)
+        # enc_output = self.item2hid(enc_output)
 
-        enc_output = self.input_dropout(enc_output)
-        enc_output = enc_output.view(batch_size,seq_len,self.d_model)
+        # enc_output = self.input_dropout(enc_output)
+        # enc_output = enc_output.view(batch_size,seq_len,self.d_model)
 
 
         for i,enc_layer in enumerate(self.layer_stack):
-            enc_output, enc_slf_attn = enc_layer(enc_output, src_user_emb,non_pad_mask=non_pad_mask, slf_attn_mask=slf_attn_mask)
+            enc_output, enc_slf_attn = enc_layer(enc_output,non_pad_mask=non_pad_mask, slf_attn_mask=slf_attn_mask)
             if return_attns:
                 enc_slf_attn_list += enc_slf_attn
 
+        if include_user:
+            enc_output = enc_output + src_user_emb ## Adding user embedding for personalization
 
         if return_attns:
-            return enc_output[:,:-1,:],enc_output[:,-1:,:], enc_slf_attn_list
+            return enc_output,enc_slf_attn_list
         
-        return enc_output[:,:-1,:],enc_output[:,-1:,:],
+        return enc_output,
 
 
     def _get_negative_prediction(self, shape, user_representation):
