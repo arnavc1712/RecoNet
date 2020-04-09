4bd075d11e9bb7b51b526b7511c5fcda14e76408
arnavc1712 <arnav171296@gmail.com>
2020-04-08 23:09:42 -0700

    Added test code

---
+++ test.py
@@ -0,0 +1,108 @@
+import torch
+import torch.optim as optim
+import opts
+from torch.utils.data import DataLoader
+from utils.utils import *
+from data.data_loader import RecDataset,rec_collate_fn
+from model.transformer.recModel import Encoder
+import torch.optim as optim
+from losses import hinge_loss, adaptive_hinge_loss,binary_cross_entropy
+from model.transformer.Optim import ScheduledOptim
+import os
+
+def test(loader,model,opt):
+	model.eval()
+	ix_to_item = loader.dataset.get_ix_to_item()
+	item_to_ix = loader.dataset.get_item_to_ix()
+	total = len(loader)
+	print("total = ", total)
+	true_pred = 0
+	total = 0
+	for i,(input_ids,target_ids,user_ids) in enumerate(loader):
+		total += input_ids.shape[0]
+		src_pos = pos_generate(input_ids)
+		user_rep,attns = model.user_representation(input_ids,src_pos,user_ids,return_attns=True,include_user=opt['include_user'])
+		target_ids=target_ids.cpu()
+		input_ids = input_ids.cpu()
+
+		tgt_indices_bf_padding = []
+		inpt_indices_bf_padding = []
+		for i in range(len(target_ids)):
+			# print(input_ids[i])
+			if len((input_ids[i]==0).nonzero().flatten()):
+				inpt_indices_bf_padding.append((target_ids[i]==0).nonzero().flatten()[0].item()-1)
+				tgt_indices_bf_padding.append((target_ids[i]==0).nonzero().flatten()[0].item()-1)
+				# print(input_ids[i])
+				# print(inpt_indices_bf_padding[i])
+			else:
+				inpt_indices_bf_padding.append(len(target_ids[i])-1)
+				tgt_indices_bf_padding.append(len(target_ids[i])-1)
+			# print((input_ids[i]==0).nonzero().flatten())
+
+		inpt_indices_bf_padding = np.array(inpt_indices_bf_padding)
+		tgt_indices_bf_padding = np.array(tgt_indices_bf_padding)
+	
+		user_rep_temp = np.expand_dims(user_rep.detach().numpy()[np.array(list(range(len(input_ids)))),inpt_indices_bf_padding,:],axis=1)
+		user_rep_temp = torch.from_numpy(user_rep_temp).type(torch.FloatTensor)
+		
+		targets = target_ids.detach().numpy()[np.array(list(range(len(target_ids)))),tgt_indices_bf_padding]
+		targets = torch.from_numpy(targets).type(torch.LongTensor)
+
+	
+		btch_sz,length,dim = user_rep_temp.size()
+		item_ids = np.array(list(ix_to_item.keys())).reshape(-1,1)
+		item_ids = torch.from_numpy(item_ids).type(torch.LongTensor).unsqueeze(0).repeat(btch_sz,1,1).cuda()
+		# print(item_ids.shape)
+		# size = (len(item_ids),) + user_rep_temp.size()
+		user_rep_temp = user_rep_temp.unsqueeze(1).repeat(1,item_ids.shape[1],1,1)
+		# print(user_rep_temp.size())
+		out = model(user_rep_temp,item_ids)
+		# print(out.shape)
+		preds = []
+		for scores in out:
+			preds.append(scores.detach().cpu().numpy().flatten())
+		# print(preds[0:10])
+
+		for i,predictions in enumerate(preds):
+			most_probable_10 = predictions.argsort()[-opt['num_recs']:][::-1]
+			most_prob_10_items = list(map(lambda x:ix_to_item[x],most_probable_10))
+			g_t = targets[i].detach().numpy().flatten()[0]
+
+			if g_t in most_probable_10:
+				true_pred+=1
+
+
+	print(f"Total: {total} ")
+	print("Model Accuracy = ", true_pred/total)
+			
+
+
+
+
+def main(opt):
+	dataset = RecDataset('test',opt)
+	dataloader = DataLoader(dataset,batch_size=128,shuffle=True,collate_fn=rec_collate_fn)
+	print(f"Number of Items: {dataset.get_num_items()}")
+	model = Encoder(seq_len=opt['max_seq_len'],
+			n_users=dataset.get_num_users(),
+			dim_user=opt["dim_item"],
+            dim_item=opt["dim_item"],
+            n_items=dataset.get_num_items(),
+            n_layers=opt["num_layer"],
+            n_head=opt["num_head"],
+            d_k=opt["dim_model"]//opt["num_head"],
+            d_v=opt["dim_model"]//opt["num_head"],
+            d_model=opt["dim_model"],
+            d_inner=opt["dim_inner"],
+            input_dropout_p=opt["input_dropout_p"],
+            dropout=opt["dropout"])
+	model.load_state_dict(torch.load(opt['load_checkpoint']))
+	model.eval()
+	test(dataloader,model,opt)
+
+
+if __name__ == "__main__":
+	print("Running")
+	opt = opts.parse_opt()
+	opt = vars(opt)
+	main(opt)
