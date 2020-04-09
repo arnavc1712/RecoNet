42dca8e4926ff288c64b14e7efe31710c09c05bd
arnavc1712 <arnav171296@gmail.com>
2020-04-08 17:07:22 -0700

    No updating learning rate

--- model/transformer/Optim.py
+++ model/transformer/Optim.py
@@ -9,7 +9,8 @@ class ScheduledOptim():
         self._optimizer = optimizer
         self.n_warmup_steps = n_warmup_steps
         self.n_current_steps = 0
-        self.init_lr = np.power(d_model, -0.5)
+        # self.init_lr = np.power(d_model, -0.5)
+        self.init_lr = 0.001
 
     def step_and_update_lr(self):
         "Step with the inner optimizer"
@@ -20,18 +21,18 @@ class ScheduledOptim():
         "Zero out the gradients by the inner optimizer"
         self._optimizer.zero_grad()
 
-    def _get_lr_scale(self):
-        return np.min([
-            np.power(self.n_current_steps, -.8),
-            # np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
-            np.power(self.n_warmup_steps, -1.8) * self.n_current_steps])
+    # def _get_lr_scale(self):
+    #     return np.min([
+    #         np.power(self.n_current_steps, -.8),
+    #         # np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+    #         np.power(self.n_warmup_steps, -1.8) * self.n_current_steps])
 
-    def _update_learning_rate(self):
-        ''' Learning rate scheduling per step '''
+    # def _update_learning_rate(self):
+    #     ''' Learning rate scheduling per step '''
 
-        self.n_current_steps += 1
-        lr = self.init_lr * self._get_lr_scale()
+    #     self.n_current_steps += 1
+    #     lr = self.init_lr * self._get_lr_scale()
 
-        for param_group in self._optimizer.param_groups:
-            param_group['lr'] = lr
+    #     for param_group in self._optimizer.param_groups:
+    #         param_group['lr'] = lr
 
