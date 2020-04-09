cb1853f5e68aea98e56ca0a38116b5a13df29f44
arnavc1712 <arnav171296@gmail.com>
2020-04-09 14:15:53 -0700

    Fixing test

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ def test(loader,model,opt):
 	total = 0
 	for i,(input_ids,target_ids,user_ids) in enumerate(loader):
 		neg_items = sample_items(num_items=num_items,shape=(100))
-		print(neg_items).shape
+		print(neg_items.shape)
 		total += input_ids.shape[0]
 		src_pos = pos_generate(input_ids)
 		input_ids = input_ids.cuda()
