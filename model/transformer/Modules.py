d41cf6c5d541414356eeecd93a7f87b52bd27094
arnavc1712 <arnav171296@gmail.com>
2020-04-09 03:02:15 -0700

    Checking nan

--- model/transformer/Modules.py
+++ model/transformer/Modules.py
@@ -23,13 +23,14 @@ class ScaledDotProductAttention(nn.Module):
         attn = torch.bmm(q, k.transpose(1, 2))
         attn = attn / self.temperature
 
-        print("ATTENTION")
-        print(attn)
+
         if mask is not None:
             attn = attn.masked_fill(mask, -np.inf)
 
         attn = self.softmax(attn)
         attn = self.dropout(attn)
+        print("ATTENTION")
+        print(attn)
         
         output = torch.bmm(attn, v)
 
